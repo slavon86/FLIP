53b5df801c911322935159c58bf2cf849aa50438
Viacheslav Chykov <viacheslav.chykov@gmail.com>
2021-05-02 02:01:22 +0300

    Improve cards layout

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,16 @@ import { shuffleOfArray } from "./helpers";
 
 function generateRandomPairs(
     inputSigns: string,
-    numberOfPairs: number,
+    difficulty?: string,
     shuffleInput?: boolean
 ): Array<string> {
+    let numberOfPairs = 8;
+    if (difficulty === "medium") {
+        numberOfPairs = 18;
+    }
+    if (difficulty === "hard") {
+        numberOfPairs = 32;
+    }
     if (inputSigns.length < numberOfPairs) {
         throw "Incorrect string of signs or number of pairs";
     }
@@ -19,18 +26,20 @@ function generateRandomPairs(
     return shuffleOfArray(outputSigns);
 }
 
-const signs = "☽☂☏☆☀☁☃☺ABCD";
-const pairs = 8;
-const cards: Card[] = generateRandomPairs(signs, pairs, true).map((sign) => {
-    const result = new Card();
-    result.sign = sign;
-    result.inGame = true;
-    result.isFlipped = true;
-    return result;
-});
+const signs = "☽☂☏☆☀☁☃☺1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
+const difficultyOfGame = "hard";
+const cards: Card[] = generateRandomPairs(signs, difficultyOfGame, true).map(
+    (sign) => {
+        const result = new Card();
+        result.sign = sign;
+        result.inGame = true;
+        result.isFlipped = true;
+        return result;
+    }
+);
 
 document.onreadystatechange = function () {
     if (document.readyState === "complete") {
-        renderCards(cards);
+        renderCards(cards, difficultyOfGame);
     }
 };
--- src/render.ts
+++ src/render.ts
@@ -1,9 +1,19 @@
 import { Card } from "./state";
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export function renderCards(cards: Card[]): void {
-    const gameElement = document.body.querySelector(".game");
+export function renderCards(cards: Card[], difficulty?: string): void {
+    const gameElement = document.body.querySelector(".field");
     if (gameElement !== null) {
         gameElement.textContent = ""; // clear the field
+        switch (difficulty) {
+            case "medium":
+                gameElement.className += " medium";
+                break;
+            case "hard":
+                gameElement.className += " hard";
+                break;
+            default:
+                gameElement.className += " easy";
+        }
         cards.forEach((card) => {
             // create cards in accord with the array of cards
             const newCardElement = document.createElement("div");
